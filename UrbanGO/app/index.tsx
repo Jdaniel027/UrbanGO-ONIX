39e4317a1d3bc281d9ace399034541003f42d082
Daniel <jose2006da@gmail.com>
2026-01-24 14:24:52 -0700

    Feat: Se agrego la pantalla de index dentro de la carpeta app

---
+++ UrbanGO/app/index.tsx
@@ -0,0 +1,25 @@
+import {Text, View, StyleSheet} from 'react-native'
+
+function index() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}> UrbanGO </Text>
+        </View>
+    )
+}
+
+export default index
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#0F172A', 
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        color: '#FFFFFF',
+        fontSize: 32,
+        fontWeight: '700',
+    },
+});
